61c9c8d1075274e5c817911aa9b942f71358ecaf
bartosz0 <methkill@gmail.com>
2021-05-18 13:16:26 +0200

    Add project files.

---
+++ Controllers/AppointmentController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            //string dateNow = DateTime.Now.ToShortDateString();
+            //return Ok(dateNow);
+
+            return View();
+        }
+        public IActionResult Detail(int id)
+        {
+            return Ok("You have entered id = " + id);
+        }
+    }
+}
